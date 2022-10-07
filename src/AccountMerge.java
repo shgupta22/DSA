e7f3dae33adfcb5154de41f301620566841c4c3b
shgupta22 <shubhamkumargupta09@gmail.com>
2022-10-07 09:28:31 +0530

    Merging Accounts

---
+++ src/AccountMerge.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class AccountMerge {
+
+    public static List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, TreeSet<String>> map = new HashMap<>();
+
+        for (List<String> accDetails: accounts) {
+            String name = accDetails.get(0);
+            for (int i = 1; i < accDetails.size(); i++) {
+                if (!map.containsKey(accDetails.get(i))) map.put(accDetails.get(i), new TreeSet<>());
+                map.get(accDetails.get(i)).addAll(accDetails);
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
