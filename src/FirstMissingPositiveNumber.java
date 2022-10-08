02d0965cb91d615e0b0d38849974e9de361e7b79
shgupta22 <shubhamkumargupta09@gmail.com>
2022-10-08 13:15:08 +0530

    Find First Positive missing number

---
+++ src/FirstMissingPositiveNumber.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+
+public class FirstMissingPositiveNumber {
+
+    /*
+        there are 3 approaches
+        1) Sort and find the missing number O(nlogn)
+        2) Second use HashSet and find the missing number O(n) with space O(n)
+
+        3) Without using extra space, use same array as a hashset O(n)
+     */
+    public static int firstMissingPositiveNumber(int[] A) {
+        int n = A.length;
+        for (int i=0; i < n; i++) {
+            if (A[i] < 0) A[i] = 0;
+        }
+
+        for (int i =0; i < n;i++) {
+            int val = Math.abs(A[i]);
+            if (val >= 1 && val <= n) {
+                if (A[val -1] > 0) {
+                    A[val-1] *= -1;
+                } else if (A[val - 1] == 0) {
+                    A[val-1] = -1* (n+1);
+                }
+            }
+        }
+
+        for (int i =1; i < n+1; i++) {
+            if (A[i-1] >= 0) return i;
+        }
+        return n+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositiveNumber(new int[]{1,2,0}));
+    }
+}
