24401723af94c44128e4db07bec47d28194f815b
shgupta22 <shubhamkumargupta09@gmail.com>
2022-10-19 09:37:58 +0530

    TopKGrequentWord Using Map and PriorityQueue

---
+++ src/TopKFrequentWords.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class TopKFrequentWords {
+
+    public static List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new TreeMap<>();
+
+        for (String word: words) {
+            Integer val = map.getOrDefault(word, 0);
+            map.put(word, ++val);
+        }
+
+        Queue<Map.Entry<String, Integer>> queue = new PriorityQueue<>((a,b) -> {
+            if (Objects.equals(b.getValue(), a.getValue())) {
+                return a.getKey().compareToIgnoreCase(b.getKey());
+            } else {
+                return b.getValue() - a.getValue();
+            }
+        });
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            queue.add(entry);
+        }
+
+        List<String> result = new ArrayList<>();
+        while (k > 0 && !queue.isEmpty()) {
+            Map.Entry<String, Integer> entry = queue.poll();
+            result.add(entry.getKey());
+            k--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(topKFrequent(new String[]{"i","love","leetcode","i","love","coding"}, 3));
+        System.out.println(topKFrequent(new String[]{"the","day","is","sunny","the","the","the","sunny","is","is"}, 4));
+    }
+}
